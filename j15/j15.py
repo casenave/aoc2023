0d7a9e23a90bd7505dde1d22b495e6277cce555d
Fabien Casenave <fabien.casenave@gmail.com>
2023-12-15 18:41:00 +0100

    res day 15

--- j15/j15.py
+++ j15/j15.py
@@ -3,16 +3,45 @@
 with open('j15_input.txt') as f:
     input = f.readlines()
 
-res = 0
+code = input[0].split(',')
 
+
+def convert(istr):
+    res = 0
+    for c in istr:
+        res += ord(c)
+        res *= 17
+        res = res%256
+    return res
+
+res = 0
+for istr in code:
+    res += convert(istr)
 
 print('res part 1 =', res)
 
 
 
+boxes = {}
+for i in range(256):
+    boxes[i] = {}
+
+for istr in code:
+    if '=' in istr:
+        label = istr.split('=')[0]
+        box = convert(label)
+        focal = int(istr.split('=')[1])
+        boxes[box][label] = focal
+    elif '-' in istr:
+        label = istr.split('-')[0]
+        box = convert(label)
+        if label in boxes[box]:
+            boxes[box].pop(label)
 
 res = 0
-
+for i, box in boxes.items():
+    for j, focal in enumerate(box.values()):
+        res += (i+1)*(j+1)*focal
 
 print('res part 2 =', res)
     
