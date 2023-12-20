bd3d24045f8150d767b1a3bbb8230cdf80e387bf
Fabien Casenave <fabien.casenave@gmail.com>
2023-12-20 14:34:29 +0100

    res day 20

--- j20/j20.py
+++ j20/j20.py
@@ -1,18 +1,58 @@
+print("DISCLAMER: solution taken from https://topaz.github.io/paste/#XQAAAQAxBQAAAAAAAAAzHIoib6pXbueH4X9F244lVRDcOZab5q1+VXY/ex42qR7D+RJIst2kujM5bGuOD3oaz/6Pv64SYp4BPkcF/7KxTVGvEJxxwMxAiDKNcmos1j8t62RwbNmhOw50Lq9qCl/Ga8E3QuTvNLkvRXgvq4gJ/3OOvXyu2XUYN4NFeYos8GftDMIYYSnC6jQ2yeK+iULXzTyl/98ite7K0+0qlsfOSYGKKNK5TQzTWynCD+ViwaokYOFxiX9zkvELdBRKcTsoIpqAOqGjl+6dbYJSrH/6GZ6rLVMY8t5pK2hRE26v7rNOhHWCThxAQ0ztvtPvxdfkryB3SVMq+bbnkpYBGwwWpdERRrOJVcmATNEmr889YkLtlFvVPMqTzZKuvfKPQsU2dbWNfLQssvRDJ51/mxkCRuTs1dtB0MLJ+z6xTtLljBBCI6tJmjT1BrB04xiRcWm80SdH8EivMpIKuzYsP6evqv7SwD8meqJDdBue93hioPdm281oxHT8LrL7eJdFIcQS/TrYiXoLU7T6OCt51s5SOR/KcxFXoEUdQ2AK2k2bqcSl9tsK90At8h8pOA+rGNwSbZVfDrrgqlun/MaRi5vppGw8SvndPRo15aoGPpVpyIdmjidhLLKXo87PlvOCoW7hN8WLV2+qVc8O3pt0S5tzydtYI0r8WN6IC5xA/xdMPwVyzO7UlzaBcx5/9fDFi2t7NyekR1ND3hD9QcVy9aD5bFBjHdzqcphonCUjR1pqgzK4STOEFVAoH30R7Dw/dBzHppuZczrUXn+grJR4YMcK4A22tuTdPeu0X/8wHFIA\n=========")
+
+from collections import defaultdict
+from math import prod
 
 
-with open('j20_input.txt') as f:
-    input = f.readlines()
+modules = dict()
+flips = defaultdict(int)
+conjs = defaultdict(dict)
 
-res = 0
+for line in open('j20_input.txt'):
+    s, _, *ds = line.replace(',', '').split()
+    t, s = (s[0], s[1:]) if s[0] in '%&' else ('', s)
 
+    modules[s] = t, ds
 
-print('res part 1 =', res)
+    for d in ds:
+        conjs[d][s] = 0
+        if d == 'rx': rx = s
 
+rx_ins = {i: 0 for i in conjs[rx]}
 
+presses = 0
+counts = [0, 0]
 
+while True:
+    if presses == 1000:
+        print('res part 1 =', prod(counts))
+    presses += 1
 
-res = 0
+    if all(rx_ins.values()):
+        print('res part 2 =', prod(rx_ins.values()))
+        break
 
+    queue = [(None, 'broadcaster', 0)]
+    while queue:
+        source, mod, pulse_in = queue.pop(0)
+        counts[pulse_in] += 1
 
-print('res part 2 =', res)
-    
+        if mod not in modules: continue
+        type, nexts = modules[mod]
+
+        match type, pulse_in:
+            case '', _:
+                pulse_out = pulse_in
+            case '%', 0:
+                pulse_out = flips[mod] = not flips[mod]
+            case '&', _:
+                conjs[mod][source] = pulse_in
+                pulse_out = not all(conjs[mod].values())
+
+                if 'rx' in nexts:
+                    for k, v in conjs[mod].items():
+                        if v: rx_ins[k] = presses
+            case _,_: continue
+
+        for n in nexts:
+            queue.append((mod, n, pulse_out))
---
+++ j20/j20_not_working.py
@@ -0,0 +1,207 @@
+print("required python >=3.10\n======")
+
+import numpy as np
+import io
+
+def run_part_1(data_string):
+
+    input = io.StringIO(data_string).readlines()
+
+    destinations = {}
+    types = {}
+    states = {}
+    origins = {}
+    latest_pulses = {}
+
+
+    for line in input:
+        l = line.split(" -> ")
+
+        key = l[0][1:].strip()
+        state = None
+
+        match l[0][0]:
+            case "b":
+                key = "broadcaster"
+                type = "broadcaster"
+            case "%":
+                type = "%"
+                state = "off"
+            case "&":
+                type = "&"
+
+
+        destinations[key] = [a.strip() for a in l[1].split(", ")]
+        types[key] = type
+        states[key] = state
+        latest_pulses[key] = "low"
+        origins[key] = []
+
+
+    for key, dest in destinations.items():
+        if type == "&":
+            for d in dest:
+                origins.setdefault(d, []).append(key)
+
+
+    def update(pulse_intensity, dest):
+        lp = pulse_intensity
+        new_pulses = []
+        count_high = 0
+        count_low = 0
+        for d in dest:
+            if pulse_intensity == "high":
+                count_high += 1
+            else:
+                count_low += 1
+            new_pulses.append([d, pulse_intensity])
+        # print("input =", location, pulse_intensity)
+        # print("output =", new_pulses, latest_pulses, count_high, count_low)
+        # print("====")
+        return new_pulses, lp, count_high, count_low
+
+    def treat_pulses(location, lp, energy, type, state, latest_pulses, dest, origs):
+
+        # print("location, energy =", location, energy)
+        # print("state, destinations =", states[location], destinations[location])
+
+        new_pulses = []
+        count_high = 0
+        count_low = 0
+
+        match type:
+            case "broadcaster":
+                assert energy == "low"
+                new_pulses = [[d, "low"] for d in dest]
+                for _ in dest:
+                    count_low += 1
+            case "%":
+                match energy:
+                    case "high":
+                        # pulses, latest_pulses, string = update(pulses, latest_pulses, string, destinations, location, "high")
+                        # for d in destinations[location]:
+                        #     string += location+" -high-> "+d+"\n"
+                        True
+                    case "low":
+                        match state:
+                            case "on":
+                                state = "off"
+                                new_pulses, lp, count_high, count_low = update("low", dest)
+                                # for d in destinations[location]:
+                                #     pulses.append([d, "low"])
+                                #     latest_pulses[d] = "low"
+                                #     string += location+" -low-> "+d+"\n"
+                            case "off":
+                                state = "on"
+                                new_pulses, lp, count_high, count_low = update("high", dest)
+                                # for d in destinations[location]:
+                                #     pulses.append([d, "high"])
+                                #     latest_pulses[d] = "high"
+                                #     string += location+" -high-> "+d+"\n"
+                        # print("%, location, states[location] =", location, states[location])
+
+            case "&":
+                # condition = np.all([True for a in origins[location] if latest_pulses[a] == "high"])
+                # print(">>", origins[location], [latest_pulses[a] for a in origins[location]])
+                # print(condition, location, destinations[location])
+                # print("latest_pulses of origins", location, origins[location], "=", [latest_pulses[a] for a in origins[location]])
+                # print("WW", np.all([latest_pulses[a] for a in origins[location]]==len(origins[location])*["high"]))
+                if np.all([latest_pulses[a] for a in origs]==len(origs)*["high"]):
+                    new_pulses, lp, count_high, count_low = update("low", dest)
+                else:
+                    new_pulses, lp, count_high, count_low = update("high", dest)
+                # print(pulses)
+
+
+        # pulses = pulses[1:]
+        # print(">>", pulses, states[location])
+        # print(string)
+        # print("===")
+        # pulses = pulses[len(destinations[location]):]
+        # print(len(pulses))
+        return new_pulses, state, lp, latest_pulses, count_high, count_low
+
+
+
+
+    nb_pushes = 1000
+
+
+    total_count_high = 0
+    total_count_low = 0
+    # for _ in tqdm(range(nb_pushes)):
+    for _ in range(nb_pushes):
+
+        pulses = [["broadcaster", "low"]]
+        total_count_low += 1
+        string = "button -low-> broadcaster\n"
+
+        count = 0
+
+
+
+        while len(pulses) > 0:
+
+            # print("test =", len(pulses), len([p for p in pulses if p[0] == 'rx']) == len(pulses), pulses, [p for p in pulses if p[0] == 'rx'])
+
+            location = pulses[0][0]
+            energy = pulses[0][1]
+
+            if location in types:
+                
+                type = types[location]
+                state = states[location]
+
+                origs = origins[location]
+                dest = destinations[location]
+                lp = latest_pulses[location]
+
+                new_pulses, state, lp, latest_pulses, count_high, count_low = treat_pulses(location, lp, energy, type, state, latest_pulses, dest, origs)
+
+                states[location] = state
+                latest_pulses[location] = lp
+
+                pulses += new_pulses
+
+                total_count_high += count_high
+                total_count_low += count_low
+
+            pulses.pop(0)
+
+
+        # total_count_high += local_count_high
+        # total_count_low += local_count_low
+
+        # count_high += pulses_count["high"]
+        # count_low += pulses_count["low"]
+        # print(pulses)
+        # print(string)
+        # # out_pulses = treat_pulses(out_pulses[0])
+        # # print(out_pulses)
+
+        # print("flip-flop state =", [(k,v) for k, v in states.items() if types[k]=="%"])
+        # print("====")
+        # print("pulses_count =", pulses_count)
+
+    return total_count_high*total_count_low
+
+
+EXAMPLE_1 = """broadcaster -> a, b, c
+%a -> b
+%b -> c
+%c -> inv
+&inv -> a
+"""
+
+EXAMPLE_2 = """broadcaster -> a
+%a -> inv, con
+&inv -> b
+%b -> con
+&con -> output
+"""
+
+print('assert part 1 example 1 correct ?', run_part_1(EXAMPLE_1)==32000000)
+print('assert part 1 example 2 correct ?', run_part_1(EXAMPLE_2)==11687500)
+
+
+    
