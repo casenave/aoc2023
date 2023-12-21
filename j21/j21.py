2d3881dd65bec8f05e744a2d519b93adaa3c1e23
Fabien Casenave <fabien.casenave@gmail.com>
2023-12-21 19:48:11 +0100

    day 21 solution from reddit

--- j21/j21.py
+++ j21/j21.py
@@ -124,3 +124,34 @@ print('assert part 2 example 1 (100 steps) correct ?', run_part_2(100, EXAMPLE_P
 # print('assert part 2 example 1 (500 steps) correct ?', run_part_2(500, EXAMPLE_PART_1)==167004)
 # print('assert part 2 example 1 (1000 steps) correct ?', run_part_2(1000, EXAMPLE_PART_1)==668697)
 # print('assert part 2 example 1 (5000 steps) correct ?', run_part_2(5000, EXAMPLE_PART_1)==16733044)
+
+
+def run_part_2_working():
+    print("DISCLAIMER: solution by 'OilAppropriate2827'")
+
+    with open('j21_input.txt') as f:
+        data = f.readlines()
+
+    n = len(data)
+    sparse = {(i,j) for i in range(n) for j in range(n) if data[i][j] in '.S'}
+    S = next((i,j) for i in range(n) for j in range(n) if data[i][j] == 'S')
+    dirs = [(1,0),(-1,0),(0,1),(0,-1)]
+
+    def tadd(a,b): return ((a[0]+b[0]),(a[1]+b[1]))
+    def modp(a): return(a[0]%n, a[1]%n)
+
+    visited, new, cache = {S}, {S}, {0:1}
+    k, r  = 26501365//n, 26501365%n
+
+    for c in range(1,r+2*n+1):
+        visited, new = new, { np for p in new for di in dirs for np in [tadd(p,di)] 
+        if np not in visited and modp(np) in sparse}
+        cache[c] = len(new) + (cache[c-2] if c>1 else 0)
+
+    d2 = cache[r+2*n]+cache[r]-2*cache[r+n]
+    d1 = cache[r+2*n]-cache[r+n]
+
+    return cache[r+2*n]+(k-2)*(2*d1+(k-1)*d2)//2
+
+print("===")
+print('res part 1 =', run_part_2_working())
